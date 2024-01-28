de0c88275b25ab674eda9975c2440474809651d3
jmlog <jmlog@gmail.com>
2024-01-28 16:18:52 +0900

    [20240128] JM : 리턴타입 스트링 추가1

--- app/Helpers/regex.php
+++ app/Helpers/regex.php
@@ -4,6 +4,7 @@ function regex_email() {
     return '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i';
 }
 
-function regex_password() {
+function regex_password(): string
+{
     return '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$/';
 }
